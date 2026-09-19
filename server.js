d0260d2b940082dc592121ab8f49b84c2d1fa306
Ax3chu <achuthraj33@gmail.com>
2026-09-19 16:04:58 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,110 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static("public"));
+
+let waitingUser = null;
+
+io.on("connection", (socket) => {
+
+    console.log("User connected:", socket.id);
+
+    socket.on("joinChat", () => {
+
+        if (waitingUser && waitingUser !== socket.id) {
+
+            const stranger = waitingUser;
+
+            waitingUser = null;
+
+            socket.partner = stranger;
+            io.sockets.sockets.get(stranger).partner = socket.id;
+
+            io.to(socket.id).emit("matched");
+
+            io.to(stranger).emit("matched");
+
+        } else {
+
+            waitingUser = socket.id;
+
+            socket.emit("waiting");
+
+        }
+
+    });
+
+
+    socket.on("message", (message) => {
+
+        const partner = socket.partner;
+
+        if (partner) {
+
+            io.to(partner).emit("message", message);
+
+        }
+
+    });
+
+
+    socket.on("next", () => {
+
+        const partner = socket.partner;
+
+        if (partner) {
+
+            io.to(partner).emit("partnerLeft");
+
+            const partnerSocket =
+                io.sockets.sockets.get(partner);
+
+            if (partnerSocket) {
+                partnerSocket.partner = null;
+            }
+
+        }
+
+        socket.partner = null;
+
+        waitingUser = socket.id;
+
+        socket.emit("waiting");
+
+    });
+
+
+    socket.on("disconnect", () => {
+
+        if (socket.partner) {
+
+            io.to(socket.partner)
+                .emit("partnerLeft");
+
+        }
+
+        if (waitingUser === socket.id) {
+
+            waitingUser = null;
+
+        }
+
+        console.log("User disconnected:", socket.id);
+
+    });
+
+});
+
+
+server.listen(3000, () => {
+
+    console.log(
+        "StrangerChat server running on port 3000"
+    );
+
+});
